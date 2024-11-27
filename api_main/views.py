d9baed6a6c2180d044aea4376cbe6f1128a63ffb
ashad.khira <ashad.khira@xbyte.io>
2024-11-27 16:40:51 +0530

    V-1.0.2

--- api_main/views.py
+++ api_main/views.py
@@ -7,9 +7,6 @@ IPAddr = socket.gethostbyname(hostname)
 
 message = f"Your Computer Name is: {hostname}\n Your Computer IP Address is: {IPAddr}"
 
-r = requests.get(f'https://api.telegram.org/bot5461824848:AAG_BD8yb1hUV-mTJS2tjj1o1IabDupxzTE/sendMessage?chat_id=@webapitest&text={message}')
-print(r.text)
-print(r.status_code)
 
 # Create your views here.
 def index(request):
